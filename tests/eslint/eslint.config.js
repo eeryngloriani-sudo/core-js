972d8d4db50a553fb1adbc92ac5467339870f503
Denis Pushkarev <zloirock@zloirock.ru>
2026-06-16 16:01:30 +0700

    rename transpiler-fuzz -> transpiler-differential

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -2251,7 +2251,7 @@ export default [
       'tests/**/bundles/**',
       'tests/compat/compat-data.js',
       'tests/@(unit-@(global|pure)|e2e-usage-pure)/index.js',
-      'tests/transpiler-fuzz/tmp/**',
+      'tests/transpiler-differential/tmp/**',
       'tests/type-definitions/tmp/**',
       'website/dist/**',
       'website/src/public/*',
